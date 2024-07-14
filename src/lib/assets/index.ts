432bc2694b4a14c6e2c5eb64099cd8ad1d53017e
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2024-07-14 18:26:16 +0300

    add favicon

--- src/lib/assets/index.ts
+++ src/lib/assets/index.ts
@@ -1,3 +1,4 @@
 import TestimonialProfileImg from '$lib/assets/testimonialCard/testimonialProfile.png';
+import Favicon from '$lib/assets/favicon.ico';
 
-export { TestimonialProfileImg };
+export { TestimonialProfileImg, Favicon };
