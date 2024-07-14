68d54c5aa7b369902e44fb22d5db270e86bc90ac
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2024-07-14 19:30:35 +0300

    refactor testimonial-card

--- src/routes/beginner/testimonial-card/+page.svelte
+++ src/routes/beginner/testimonial-card/+page.svelte
@@ -4,26 +4,26 @@
 	import '@fontsource-variable/noto-sans';
 
 	const { data } = $props();
-	const defaultMeta = data.meta;
+	const meta = data.meta;
 	const testimonial = data.testimonialCard;
 </script>
 
 <svelte:head>
-	<title>{defaultMeta.title}</title>
+	<title>{meta.title}</title>
 
-	<meta property="og:title" content={defaultMeta.title} />
-	<meta property="og:description" content={defaultMeta.description} />
+	<meta property="og:title" content={meta.title} />
+	<meta property="og:description" content={meta.description} />
 
 	<meta property="og:type" content="website" />
-	<meta property="og:site_name" content={defaultMeta.title} />
-	<meta property="og:url" content={defaultMeta.title} />
+	<meta property="og:site_name" content={meta.title} />
+	<meta property="og:url" content={meta.title} />
 
-	<meta property="og:image" content={defaultMeta.ogImageUrl} />
+	<meta property="og:image" content={meta.ogImageUrl} />
 	<meta name="twitter:card" content="summary_large_image" />
 
 	<link rel="icon" type="image/png" href={Favicon} />
 </svelte:head>
 
-<div class="flex h-screen justify-center items-center bg-gradient-to-r from-gray-50 to-[#d2d6db]">
+<div class="default-page">
 	<TestimonialCard {testimonial} />
 </div>
