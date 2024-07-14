2fe935626ccb522c22308dd251a11462e8787d05
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2024-07-14 19:29:44 +0300

    Add BlogCardImg to assets

--- src/lib/assets/index.ts
+++ src/lib/assets/index.ts
@@ -1,4 +1,5 @@
 import TestimonialProfileImg from '$lib/assets/testimonialCard/testimonialProfile.png';
 import Favicon from '$lib/assets/favicon.ico';
+import BlogCardImg from '$lib/assets/blogCard/blogCardImg.jpg';
 
-export { TestimonialProfileImg, Favicon };
+export { TestimonialProfileImg, Favicon, BlogCardImg };
