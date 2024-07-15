cec9accd4d01a42d77aa43318fbbbdda2e44600c
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2024-07-15 12:50:37 +0300

    fix type for build

--- src/lib/assets/index.ts
+++ src/lib/assets/index.ts
@@ -1,6 +1,6 @@
 import TestimonialProfileImg from '$lib/assets/testimonialCard/testimonialProfile.png';
 import Favicon from '$lib/assets/favicon.ico';
 import BlogCardImg from '$lib/assets/blogCard/blogCardImg.jpg';
-import Prism from '$lib/assets/heroSection/prism.png';
+import Prism from '$lib/assets/heroSection/Prism.png';
 
 export { TestimonialProfileImg, Favicon, BlogCardImg, Prism };
